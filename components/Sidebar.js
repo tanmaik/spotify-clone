f798640f12e185076f0219e99335cbe7f8b9bb0b
Tanmai Kalisipudi <tanmai.kalisipudi@gmail.com>
2022-07-23 12:20:41 -0400

    imported usesession

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -7,10 +7,20 @@ import {
   RssIcon,
 } from "@heroicons/react/outline";
 
+import { signOut, useSession } from "next-auth/react";
+
 function Sidebar() {
+  const { data: session, status } = useSession();
+  console.log(session);
   return (
     <div className="text-gray-500 p-5 text-sm border-r border-grey-900">
       <div className="space-y-4">
+        <button
+          className="flex items-center space-x-2 hover:text-white"
+          onClick={() => signOut()}
+        >
+          <p>Log out</p>
+        </button>
         <button className="flex items-center space-x-2 hover:text-white">
           <HomeIcon className="h-5 w-5" />
           <p>Home</p>
@@ -24,7 +34,6 @@ function Sidebar() {
           <p>Your Library</p>
         </button>
         <hr className="border-t-[0.1px] border-gray-900" />
-
         <button className="flex items-center space-x-2 hover:text-white">
           <PlusCircleIcon className="h-5 w-5" />
           <p>Create Playlist</p>
@@ -38,7 +47,6 @@ function Sidebar() {
           <p>Your episodes</p>
         </button>
         <hr className="border-t-[0.1px] border-gray-900" />
-
         {/* Playlists */}
         <p className="cursor-pointer hover:text-white">Playlist Name...</p>
         <p className="cursor-pointer hover:text-white">Playlist Name...</p>
@@ -47,7 +55,7 @@ function Sidebar() {
         <p className="cursor-pointer hover:text-white">Playlist Name...</p>
         <p className="cursor-pointer hover:text-white">Playlist Name...</p>
         <p className="cursor-pointer hover:text-white">Playlist Name...</p>
-        <p className="cursor-pointer hover:text-white">Playlist Name...</p>s
+        <p className="cursor-pointer hover:text-white">Playlist Name...</p>
       </div>
     </div>
   );
--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -59,5 +59,12 @@ export default NextAuth({
       console.log("ACCESS TOKEN HAS EXPIRED, REFRESHING...");
       return await refreshAccessToken(token);
     },
+    async session({ session, token }) {
+      session.user.accessToken = token.accessToken;
+      session.user.refreshToken = token.refreshToken;
+      session.user.username = token.username;
+      
+      return session;
+    },
   },
 });
